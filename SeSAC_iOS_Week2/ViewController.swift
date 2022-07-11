3da9a9b8d3403c7b8531590e0b1347ec35ffd8d4
sanhan42 <sangmin613@gmail.com>
2022-07-11 15:05:31 +0900

    Initial Commit

--- SeSAC_iOS_Week2/ViewController.swift
+++ SeSAC_iOS_Week2/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var orangeView: UIView!
+    @IBOutlet weak var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        orangeView.layer.cornerRadius = orangeView.frame.width/2
+        greenView.layer.cornerRadius = 10
+        greenView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+//        orangeView.clipsToBounds = false // shadow
         // Do any additional setup after loading the view.
     }
 
